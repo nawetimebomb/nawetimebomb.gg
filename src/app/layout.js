7a3f641e733f0f2d6541c9b01c3f2c8253c8e4be
Nahuel J. Sacchetti <nahuel.sacchetti@wnco.com>
2024-01-04 21:44:28 -0600

    add preload images

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,7 @@
 import { Inter } from "next/font/google";
+
+import games from "@local/data/games.json";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,8 +12,15 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+    const { completed } = games;
+
     return (
         <html lang="en">
+            <head>
+                {completed.map((item) => (
+                    <link rel="preload" as="image" href={`backgrounds/${item.platinumNumber}.jpg`} />
+                ))}
+            </head>
             <body>{children}</body>
         </html>
     );
